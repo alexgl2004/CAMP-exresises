eaff1cfeddfc27c286c0555124daffea625b1478
Oleksii Glavatyi <alexgl.netbook2@gmail.com>
2024-01-25 18:12:12 +0100

    fertish

--- js-next-functions/map.js
+++ js-next-functions/map.js
@@ -1,24 +1,22 @@
-
 // Write a program that uses the Array map() method to transform all objects to a new Array personsWithFullName.
 
 // input data
 const persons = [
-    { firstName: 'John', lastName: 'Doe', age: 32 },
-    { firstName: 'Jane', lastName: 'Doe', age: 23 }
+  { firstName: "John", lastName: "Doe", age: 32 },
+  { firstName: "Jane", lastName: "Doe", age: 23 },
 ];
 
 // add your implementation
 //const personsWithFullName = [];
 
 const personsWithFullName = persons.map((person) => {
-    return {
-        firstName : person.firstName,
-        lastName : person.lastName,
-        age : person.age,
-        fullName : person.firstName +' ' + person.lastName
-    }
+  return {
+    firstName: person.firstName,
+    lastName: person.lastName,
+    age: person.age,
+    fullName: person.firstName + " " + person.lastName,
+  };
 });
-
 
 console.log(personsWithFullName);
 // output data should look like:
--- js-next-functions/reduce.js
+++ js-next-functions/reduce.js
@@ -2,16 +2,25 @@
 // Write a program that uses the Array reduce() method to calculate the average age of persons.
 
 const persons = [
-    { firstName: 'John', lastName: 'Doe', age: 32 },
-    { firstName: 'Jane', lastName: 'Doe', age: 23 },
+    { firstName: 'John', lastName: 'Doe', age: 25 },
+    { firstName: 'Jane', lastName: 'Doe', age: 25 },
+    { firstName: 'John', lastName: 'Doe', age: 10 },
+    { firstName: 'Jane', lastName: 'Doe', age: 25 }
 ];
 
 const initialValue = 0;
 
 let averageAgeArrSum = persons.reduce((accumulator, currentValue) => {
-     return accumulator + currentValue.age    
+     return accumulator + currentValue.age
 }, initialValue);
 
 let averageAge = averageAgeArrSum / persons.length;
 
+console.log(averageAge);
+
+averageAgeArrSum = persons.reduce((accumulator, currentValue) => {
+    return accumulator + (currentValue.age/persons.length)
+}, initialValue);
+
+averageAge = averageAgeArrSum
 console.log(averageAge);
