563c949d38dd0c2736682f63fa0d058255f3bd1f
Oleksii Glavatyi <alexgl.netbook2@gmail.com>
2024-01-29 14:33:05 +0100

    next changes

--- react-array/campus-starter-users-array-react-2/src/App.jsx
+++ react-array/campus-starter-users-array-react-2/src/App.jsx
@@ -3,14 +3,7 @@ import { Card } from "./components/Card";
 import { users } from "./data/users";
 import { useState } from 'react'
 
-let arr_of_states = {
-  All : 1,
-  Women : 0,
-  Men : 0,
-  By_name : 0,
-  By_age : 0
-};
-
+/*
 export function Make_filter_sort_user(users, class_of_clicked_button){
 
   let a = class_of_clicked_button;
@@ -22,56 +15,66 @@ export function Make_filter_sort_user(users, class_of_clicked_button){
   })
 
 }
-
-function change_object_arr_of_states(selectedButton, arr_of_states){
-  if(selectedButton=='Women'||selectedButton=='Men'){
-
-    arr_of_states['All'] = 0;
-
-    arr_of_states['Women'] = selectedButton=='Men'?0:arr_of_states['Women']
-    arr_of_states['Men'] = selectedButton=='Women'?0:arr_of_states['Men']
-  
-  }
-
-  if(selectedButton=='All'){
-    arr_of_states['Women'] = arr_of_states['Men'] = 0;
-  }
-
-  if(selectedButton=='By_age'||selectedButton=='By_name'){
-
-    arr_of_states['By_name'] = selectedButton=='By_age'?0:arr_of_states['By_name']
-    arr_of_states['By_age'] = selectedButton=='By_name'?0:arr_of_states['By_age']  
-    
-    arr_of_states[selectedButton] = arr_of_states[selectedButton] ? 0 : 1
-
-  }else{
-
-    arr_of_states[selectedButton] = 1
-
-  }
- 
-  return arr_of_states
-
-}
+*/
 
 function App() {
 
   const [selectedButton, setSelectedButton] = useState('all');
+
+  const [obj_of_states, setObj_of_states] = useState({
+    All : 1,
+    Women : 0,
+    Men : 0,
+    By_name : 0,
+    By_age : 0
+  })
   
   let users_out_temp
   let users_out_temp_sort
 
-  arr_of_states = change_object_arr_of_states(selectedButton, arr_of_states)
+  function change_object_obj_of_states(selectedButton, obj_of_states){
+    console.log(1)
+    if(selectedButton=='Women'||selectedButton=='Men'){
+  
+      obj_of_states['All'] = 0;
+  
+      obj_of_states['Women'] = selectedButton=='Men'?0:obj_of_states['Women']
+      obj_of_states['Men'] = selectedButton=='Women'?0:obj_of_states['Men']
+    
+    }
+  
+    if(selectedButton=='All'){
+      obj_of_states['Women'] = obj_of_states['Men'] = 0;
+    }
+  
+    if(selectedButton=='By_age'||selectedButton=='By_name'){
+  
+      obj_of_states['By_name'] = selectedButton=='By_age'?0:obj_of_states['By_name']
+      obj_of_states['By_age'] = selectedButton=='By_name'?0:obj_of_states['By_age']  
+      
+      obj_of_states[selectedButton] = obj_of_states[selectedButton]==1 || obj_of_states[selectedButton]==0 ? -1 : 1
+  
+    }else{
+  
+      obj_of_states[selectedButton] = 1
+  
+    }
+   
+    return obj_of_states
+  
+  }
+  
+  obj_of_states = change_object_obj_of_states(selectedButton, obj_of_states)
 
-  console.log(selectedButton,arr_of_states)
+  console.log(selectedButton,obj_of_states)
 
   //FILTER
-  if(arr_of_states.All==1){
+  if(obj_of_states.All==1){
     users_out_temp = users
   }else{
     let gender = ''
-    gender = arr_of_states.Women==1?'female':gender
-    gender = arr_of_states.Men==1?'male':gender
+    gender = obj_of_states.Women==1?'female':gender
+    gender = obj_of_states.Men==1?'male':gender
     if(gender){
       users_out_temp = users.filter((user) => {
         return user.gender == gender
@@ -91,19 +94,24 @@ function App() {
 
   //SORT
   
-  if(arr_of_states.By_name==1){
-    console.log('a')
-    users_out_temp_sort = users_out_temp.sort((a, b) => a.name.last - b.name.last)
-  }else if(arr_of_states.By_age==1){
-    users_out_temp_sort = users_out_temp.sort((a, b) => a.dob.age - b.dob.age)
-    console.log('b')
+  if(obj_of_states.By_name!=0){
+    if(obj_of_states.By_name==1){
+      users_out_temp_sort = users_out_temp.sort((a, b) => a.name.last.localeCompare(b.name.last))
+    }else{
+      users_out_temp_sort = users_out_temp.sort((a, b) => b.name.last.localeCompare(a.name.last))
+    }
+  }else if(obj_of_states.By_age!=0){
+    if(obj_of_states.By_age==1){
+      users_out_temp_sort = users_out_temp.sort((a, b) => a.dob.age - b.dob.age)
+    }else{
+      users_out_temp_sort = users_out_temp.sort((a, b) => b.dob.age - a.dob.age)
+    }
   }else{
-    users_out_temp_sort = users_out_temp.sort((a, b) => a.name.first - b.name.first)
-    console.log('c')
+    users_out_temp_sort = users_out_temp.sort((a, b) => a.name.first.localeCompare(b.name.first))
   }
   
 
-  console.log(arr_of_states.By_name+'||'+arr_of_states.By_age)
+  console.log(obj_of_states.By_name+'||'+obj_of_states.By_age)
   //console.log(users_out_temp)
 
   //OUTPUT
@@ -119,13 +127,13 @@ function App() {
       </header>
       <main>
         <section className="filters-button">
-          <button onClick={() => setSelectedButton("All")} className={arr_of_states.All==1?"active ":"" +"filter_button"}>All</button>
+          <button onClick={() => setSelectedButton("All")} className={obj_of_states.All==1?"active ":"" +"filter_button"}>All</button>
           <span className="devider">&nbsp;|&nbsp;</span>
-          <button onClick={() => setSelectedButton("Women")} className={arr_of_states.Women==1?"active ":"" +"filter_button"}>Women</button>
-          <button onClick={() => setSelectedButton("Men")} className={arr_of_states.Men==1?"active ":"" +"filter_button"}>Men</button>
+          <button onClick={() => setSelectedButton("Women")} className={obj_of_states.Women==1?"active ":"" +"filter_button"}>Women</button>
+          <button onClick={() => setSelectedButton("Men")} className={obj_of_states.Men==1?"active ":"" +"filter_button"}>Men</button>
           <span className="devider">&nbsp;|&nbsp;</span>
-          <button onClick={() => setSelectedButton("By_name")} className={arr_of_states.By_name==1?"active ":"" +"filter_button"}>By name</button>
-          <button onClick={() => setSelectedButton("By_age")} className={arr_of_states.By_age==1?"active ":"" +"filter_button"}>By age</button>
+          <button onClick={() => setSelectedButton("By_name")} className={obj_of_states.By_name!=0?"active ":"" +"filter_button"}>By name</button>
+          <button onClick={() => setSelectedButton("By_age")} className={obj_of_states.By_age!=0?"active ":"" +"filter_button"}>By age</button>
         </section>
         <div>
           <section className="card-list">
